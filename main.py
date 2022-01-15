9d4fa6183faacc6e0d6ffe619ee83c0ed03c4b22
Matheus Oliveira <matheus7op@gmail.com>
2022-01-15 18:10:31 -0300

    applies flake8 linter. changes number of hidden layers and number of nodes to match reference

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import os
 
 import librosa
-import python_speech_features
+from python_speech_features.base import mfcc
 
 from scipy.signal.windows import hamming
 import numpy as np
 
 import config
 
-# lembrete importante:
-# Rescaling the data to small values (in general, input values to a neural network should be close 
-# to zero -- typically we expect either data with zero-mean and unit-variance, or data in the [0, 1] range).
-# o keras tem modulo de normalization. talvez seja necessario usar...?
+"""
+lembrete importante:
+Rescaling the data to small values (in general, input values to a neural
+network should be close to zero -- typically we expect either data with
+zero-mean and unit-variance, or data in the [0, 1] range). o keras tem modulo
+de normalization. talvez seja necessario usar...?
+"""
+
 
 def apply_preemphasis(audio_signal: np.ndarray):
-    return librosa.effects.preemphasis(audio_signal, coef=config.PREEMPHASIS_COEFFICIENT)
+    return librosa.effects.preemphasis(
+        audio_signal,
+        coef=config.PREEMPHASIS_COEFFICIENT)
+
 
 def extract_features(audio_signal: np.ndarray):
     # extracts jitter, shimmer and MFCCs
 
-    mfcc = python_speech_features.mfcc(
-        signal=audio_signal, 
-        samplerate=config.DEFAULT_SAMPLE_RATE, 
-        winlen=config.FRAME_LENGTH / config.DEFAULT_SAMPLE_RATE, 
+    mfccs = mfcc(
+        signal=audio_signal,
+        samplerate=config.DEFAULT_SAMPLE_RATE,
+        winlen=config.FRAME_LENGTH / config.DEFAULT_SAMPLE_RATE,
         winstep=config.HOP_LENGTH / config.DEFAULT_SAMPLE_RATE,
-        numcep=config.NUM_MFCC, 
-        nfilt=config.N_MELS, 
-        nfft=config.FRAME_LENGTH, 
-        lowfreq=config.F_MIN, 
+        numcep=config.NUM_MFCC,
+        nfilt=config.N_MELS,
+        nfft=config.FRAME_LENGTH,
+        lowfreq=config.F_MIN,
         highfreq=config.F_MAX,
-        preemph=config.PREEMPHASIS_COEFFICIENT, 
-        ceplifter=config.CEPLIFTER, 
-        appendEnergy=True, 
+        preemph=config.PREEMPHASIS_COEFFICIENT,
+        ceplifter=config.CEPLIFTER,
+        appendEnergy=True,
         winfunc=hamming
     )
 
@@ -43,31 +50,54 @@ def extract_features(audio_signal: np.ndarray):
     # print(mfcc[0])
     # print(len(mfcc[0]))
 
-    # snd = parselmouth.Sound(f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav")
+    # snd = parselmouth.Sound(
+    #     f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav")
 
     # pitch = snd.to_pitch()
     # pulses = parselmouth.praat.call([snd, pitch], "To PointProcess (cc)")
 
     # # jitter. (l, r, period floor, period ceiling, maximum period factor)
-    # jitter_local = parselmouth.praat.call(pulses, "Get jitter (local)", 0.0, 0.0, 0.0001, 0.02, 1.3)
+    # jitter_local = parselmouth.praat.call(
+    #     pulses,
+    #     "Get jitter (local)",
+    #     0.0,
+    #     0.0,
+    #     0.0001,
+    #     0.02,
+    #     1.3)
 
-    # # shimmer
-    # shimmer_local = parselmouth.praat.call([snd, pulses], "Get shimmer (local)", 0.0, 0.0, 0.0001, 0.02, 1.3, 1.6)
+    # shimmer
+    # shimmer_local = parselmouth.praat.call(
+    #     [snd, pulses],
+    #     "Get shimmer (local)",
+    #     0.0,
+    #     0.0,
+    #     0.0001,
+    #     0.02,
+    #     1.3,
+    #     1.6)
 
     # https://bibliotecadigital.ipb.pt/bitstream/10198/20502/1/pauta-relatorio-43.pdf
-    # calculates jitter and shimmer as "absolute" metrics, for the whole audio signal, not its segments
+    # calculates jitter and shimmer as "absolute" metrics, for the whole audio
+    # signal, not its segments
 
-    return mfcc # jitter_local, shimmer_local off the table for now
+    return mfccs  # jitter_local, shimmer_local off the table for now
+
 
 def pre_processing(audio_signal: np.ndarray) -> np.ndarray:
     # apply pre emphasis and hamming window function
     processed_signal = apply_preemphasis(audio_signal)
 
-    frames = librosa.util.frame(processed_signal, frame_length=config.FRAME_LENGTH, hop_length=config.HOP_LENGTH)
+    frames = librosa.util.frame(
+        processed_signal,
+        frame_length=config.FRAME_LENGTH,
+        hop_length=config.HOP_LENGTH)
+
     windowed_frames = np.hamming(config.FRAME_LENGTH).reshape(-1, 1) * frames
 
     # overlapping frames with window function applied
     return windowed_frames
+
 
 # validates whether the sample rate across all the dataset is unique
 def validate_sample_rate():
@@ -81,15 +111,19 @@ def validate_sample_rate():
     if len(sample_rates) > 1:
         raise Exception("Sample rate is not unique in the dataset.")
 
+
 def normalize_input(dataset):
     maxi = 0
     for i in dataset:
-        for j in i: maxi = max(maxi, abs(j))
+        for j in i:
+            maxi = max(maxi, abs(j))
 
     for i in range(len(dataset)):
-        for j in range(len(dataset[i])): dataset[i][j] /= maxi
-    
-    #assert(dataset[i][j] <= 1)
+        for j in range(len(dataset[i])):
+            dataset[i][j] /= maxi
+
+    # assert(dataset[i][j] <= 1)
+
 
 def load_dataset():
     dataset = []
@@ -108,20 +142,32 @@ def load_dataset():
 
             dataset.append(mfccs)
 
-            if f.find("saudavel") != -1: results.append(0)
-            else: results.append(1)
+            # if len(dataset) == 0: dataset = np.copy(mfccs)
+            # else: dataset = np.append(dataset, mfccs, axis=0)
 
-    normalize_input(dataset)
+            if f.find("saudavel") != -1:
+                results.append(0)
+            else:
+                results.append(1)
+
+    # normalize_input(dataset)
     return (np.asarray(dataset), np.asarray(results))
+
 
 if __name__ == "__main__":
     validate_sample_rate()
 
-    # librosa -> load. keeps default sample rate with sr=None. each value is a sample (amplitude).
+    # librosa -> load. keeps default sample rate with sr=None. each value is
+    # a sample (amplitude).
 
-    y, _ = librosa.load(f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav", sr=None)
-    y2, _ = librosa.load(f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav", sr=None)
-    
+    y, _ = librosa.load(
+        f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav",
+        sr=None)
+
+    y2, _ = librosa.load(
+        f"{config.DATASET_PATH}/pathological/carcinoma_masculino_1.wav",
+        sr=None)
+
     extract_features(y)
     pre_processing(y2)
 
--- model.py
+++ model.py
@@ -1,14 +1,13 @@
 import numpy
 from tensorflow import keras
-from keras.layers import Flatten
 
 import config
 from main import load_dataset
 
 inp, out = load_dataset()
 
-# print(inp.shape)
-# print(out.shape)
+print(inp.shape)
+print(out.shape)
 
 training_in = []
 training_out = []
@@ -36,40 +35,68 @@ validation_in = numpy.asarray(validation_in)
 model = keras.Sequential()
 model.add(keras.Input(shape=(24*config.NUM_MFCC)))
 
+"""
+[...] sendo analisados classificadores com 04, 05 e 06 camadas ocultas.
+A motivação de alterar a quantidade de camadas ocultas está relacionada
+com o interesse de avaliar os efeitos que a profundidade da DNN causam na
+eficiência do classificador.
+
+Durante as etapas de treino e validação, o sinal de voz é fracionado em
+segmentos estacionários de 16 ms, possuindo no total 400 amostras. A camada
+de entrada da DNN recebe as 400 amostras (instâncias) referentes ao segmento
+em análise. Neste trabalho, cada camada oculta dos classificadores é composta
+por 200 neurônios [...]
+"""
+
 # first parameter is number of nodes. activation is the activation function
-model.add(keras.layers.Dense(13, activation='relu'))
-model.add(keras.layers.Dense(8, activation='relu'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(200, activation='relu'))
+model.add(keras.layers.Dense(200, activation='relu'))
+
 model.add(keras.layers.Dense(1, activation='sigmoid'))
 
 model.summary()
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
 
 # [print(i.shape, i.dtype) for i in model.inputs]
 # [print(o.shape, o.dtype) for o in model.outputs]
 # [print(l.name, l.input_shape, l.dtype) for l in model.layers]
 
-model.fit(training_in, training_out, epochs=150, batch_size=10)
+model.fit(training_in, training_out, epochs=200, batch_size=10)
 
 loss, accuracy = model.evaluate(validation_in, validation_out)
 print('Accuracy: %.2f' % (accuracy*100))
 
 # predictions = (model.predict(validation_in) > 0.5).astype(int)
 # for i in range(5):
-# 	print('%s => %s (expected %d)' % (inp[i].tolist(), predictions[i], validation_out[i]))
+#     print('%s => %s (expected %d)' % (
+#         inp[i].tolist(),
+#         predictions[i],
+#         validation_out[i]))
 
-# how does the input work? 
-# I now I'll have like 95 ndarrays (95 windows) of 13 elements each (13 coefficients for each window)
-# will I run classification on each of the 95 arrays then do something?
-# or input the whole 95 ndarrays and classify?
+"""
+how does the input work?
+I now I'll have like 95 ndarrays (95 windows) of 13 elements each (13
+coefficients for each window) will I run classification on each of the 95
+arrays then do something? or input the whole 95 ndarrays and classify?
 
-# speaking of https://bibliotecadigital.ipb.pt/bitstream/10198/20502/1/pauta-relatorio-43.pdf
-# we input the whole 95 ndarrays, but only after we turn them into a single (unidimensional) array.
-# they use tf.keras.layers.Conv1D instead of sequential?
+speaking of
+https://bibliotecadigital.ipb.pt/bitstream/10198/20502/1/pauta-relatorio-43.pdf
+we input the whole 95 ndarrays, but only after we turn them into a single
+(unidimensional) array. they use tf.keras.layers.Conv1D instead of sequential?
 
-# it is important to note that I'll need to do some trial and error to find the best
-# number of layers and number of neurons. check page 52 of the above link to get some insight.
+it is important to note that I'll need to do some trial and error to find the
+best number of layers and number of neurons. check page 52 of the above link
+to get some insight.
 
-# remember to make/do dropout to avoid overfitting (I believe this is stopping the training at certain point)
+remember to make/do dropout to avoid overfitting (I believe this is stopping
+the training at certain point)
 
-# something I'll need to do: divide the dataset into training and validation datasets. training > validation
-# and of course, divide them by result (pathological or not). (done this already) 
+something I'll need to do: divide the dataset into training and validation
+datasets. training > validation and of course, divide them by result
+(pathological or not). (done this already)
+"""
